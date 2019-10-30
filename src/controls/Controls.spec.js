08c7bba1c282760787286f61257c5668bb9e937b
Robert Kulp <pedalinon@gmail.com>
2019-10-30 13:56:41 -0700

    Dependencies

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+
+// import axios from "axios"
+import React from "react";
+// no default export, so we're importing everyting from this library
+import * as rtl from "@testing-library/react";
+// not importing to a variable, since this just overrides jest global variables
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+afterEach(rtl.cleanup);
